47f3dbb3a20be25e436e3062f3ae21d874e30971
Chubbard022 <chubbard022@gmail.com>
2019-05-13 11:21:59 -0700

    set up first test on repair method

---
+++ enhancing/enhancer.test.js
@@ -0,0 +1,10 @@
+const enhancer = require('./enhancer.js');
+// test away!
+describe("enhancer.js",()=>{
+    describe("repair() method",()=>{
+        it("should return durability restored to 100 ",()=>{
+
+            expect(repair).toBe(100)
+        })
+    })
+})
